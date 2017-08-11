b914b4e186fa247a64febbc596cb01e8d317d8a1
Daniel Sarosi <daniels@muneris.io>
2017-08-11 15:07:34 +0800

    Fixed unit tests for scaldi-play.

--- src/main/scala/scaldi/play/ControllerComponentsModule.scala
+++ src/main/scala/scaldi/play/ControllerComponentsModule.scala
@@ -3,7 +3,6 @@ package scaldi.play
 import play.api.http.FileMimeTypes
 import play.api.i18n.{Langs, MessagesApi}
 import play.api.mvc.{ControllerComponents, DefaultActionBuilder, DefaultControllerComponents, PlayBodyParsers}
-import scaldi.Injectable.inject
 import scaldi.Module
 
 import scala.concurrent.ExecutionContext
